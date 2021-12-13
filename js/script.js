c314f3d0927b1e5d40546744327de8fa31c2872a
aleksey-t <megapozitivizm@yandex.ru>
2021-12-13 22:32:11 +0300

    добавил бургер меню, фиксы по стилям

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function showLinks() {
+    document.querySelector(".links").classList.remove("hidden");
+    document.querySelector(".burger-menu").classList.add("cross-icon");
+}
+
+function hideLinks() {
+    document.querySelector(".links").classList.add("hidden");
+    document.querySelector(".burger-menu").classList.remove("cross-icon");
+}
+
+function handleClick() {
+    if (document.querySelector(".links").classList.contains("hidden")) {
+        showLinks();
+    } else {
+        hideLinks();
+    }
+}
+
+document.querySelector("#burger-menu").addEventListener("click", handleClick);
