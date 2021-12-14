e182ca99044a42f129b6309cf14edc701252a351
Urgant <john_h@list.ru>
2021-12-14 23:25:21 +0300

    добавил elsint и prettier

--- validate.js
+++ validate.js
@@ -1,16 +1,23 @@
+let nameField = document.getElementById("customerName");
+let phoneNumber = document.getElementById("phone");
+let zajavka = document.getElementById("message-box");
+let sendOrder = document.getElementById("order");
 
-
-let nameField = document.getElementById('customerName');
-let phoneNumber = document.getElementById('phone');
-let zajavka = document.getElementById('message-box');
-let sendOrder = document.getElementById('order');
-
-function checkFields () {
-
+function checkFields() {
+  if (!nameField.value.length) {
+    nameField.style.borderColor = "red";
+    console.log("эээ, слищищь. Скажи, как тебя зовут, Ара");
+  }
+  if (!phoneNumber.value.length) {
+    phoneNumber.style.borderColor = "red";
+    console.log("вай-вай! Дай по телефону позвоню!");
+  }
+  if (!zajavka.value.length) {
+    zajavka.style.borderColor = "red";
+    console.log("напиши всё, что пришло тебе в голову, дорогой друг!");
+  }
 }
-
-
-
+sendOrder.addEventListener("click", checkFields);
 
 // у кнопки id=order
 //
