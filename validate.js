f71d1d6cc11134ef51cb626def0cafa3ec15aa52
Urgant <john_h@list.ru>
2021-12-14 22:52:26 +0300

    добавил validate.js

---
+++ validate.js
@@ -0,0 +1,21 @@
+
+
+let nameField = document.getElementById('customerName');
+let phoneNumber = document.getElementById('phone');
+let zajavka = document.getElementById('message-box');
+let sendOrder = document.getElementById('order');
+
+function checkFields () {
+
+}
+
+
+
+
+// у кнопки id=order
+//
+// создать callback -функцию для события клик
+//
+// задать id полям имя телефон и текст заявки в html
+//
+// проверять длину каждого поля на введенные символы
