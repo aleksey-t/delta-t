e1e8de63d7656f4dd87861068c2fe5b93978d481
aleksey-t <megapozitivizm@yandex.ru>
2021-12-16 10:43:25 +0300

    добавил блок комплексный подход

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
 function showLinks() {
-    document.querySelector(".links").classList.remove("hidden");
-    document.querySelector(".burger-menu").classList.add("cross-icon");
+  document.querySelector(".links").classList.remove("hidden");
+  document.querySelector(".burger-menu").classList.add("cross-icon");
 }
 
 function hideLinks() {
-    document.querySelector(".links").classList.add("hidden");
-    document.querySelector(".burger-menu").classList.remove("cross-icon");
+  document.querySelector(".links").classList.add("hidden");
+  document.querySelector(".burger-menu").classList.remove("cross-icon");
 }
 
 function handleClick() {
-    if (document.querySelector(".links").classList.contains("hidden")) {
-        showLinks();
-    } else {
-        hideLinks();
-    }
+  if (document.querySelector(".links").classList.contains("hidden")) {
+    showLinks();
+  } else {
+    hideLinks();
+  }
 }
 
 document.querySelector("#burger-menu").addEventListener("click", handleClick);
+const links = document.querySelectorAll(".menu-item a");
+Array.prototype.slice
+  .call(links)
+  .map((item) => item.addEventListener("click", hideLinks));
